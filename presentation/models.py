125fa471c9838369df6b718cc1ef8005aa1e5ad8
Wonyoung Ju <getogrand@gmail.com>
2016-10-15 11:51:58 +0900

    2016.10.15 -  Introduce grid design to list page .

--- presentation/models.py
+++ presentation/models.py
@@ -6,6 +6,7 @@ from warp.users.models import User
 
 class Presentation(TimeStampedModel):
     subject = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
     markdown = models.TextField()
     html = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
