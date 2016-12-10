5ff4ff86c0872bc9ff29061d12bf49be0e6c3a97
myoungho.Pak <qkraudghgh@naver.com>
2016-12-10 09:10:29 +0900

    Rename markdown_div to jquery variable with $

--- warp/static/js/create.js
+++ warp/static/js/create.js
@@ -1,6 +1,6 @@
 $(function () {
     var before_value = '',
-        markdown_div = $('#id_markdown'),
+        $markdown_div = $('#id_markdown'),
         $preview = $('.preview'),
         $html_div = $('#id_html'),
         $create_form = $('#create_html'),
@@ -9,29 +9,29 @@ $(function () {
         markdown_slides = [];
 
     $create_form.submit(function () {
-        $html_div.val(marked(markdown_div.val()));
+        $html_div.val(marked($markdown_div.val()));
     });
 
     // onFocus 이벤트 발생 시 setInterval을 통해 _onWatch를 실행
-    markdown_div.focus(function (e) {
+    $markdown_div.focus(function (e) {
         markdown_event = setInterval($.proxy(function () {
             _onWatch();
         }), 200);
     });
 
-    markdown_div.blur(function (e) {
-        if (markdown_div) {
+    $markdown_div.blur(function (e) {
+        if ($markdown_div) {
             clearInterval(markdown_event);
         }
     });
 
     function _onWatch() {
         // 이전 값과 현재 값이 다르면 필요한 액션 수행
-        var currentValue = markdown_div.val();
+        var currentValue = $markdown_div.val();
         if (before_value !== currentValue) {
             before_value = currentValue;             // 현재 값 저장
 
-            markdown_content = markdown_div.val();
+            markdown_content = $markdown_div.val();
             markdown_slides = markdown_content.split(/={5,}/);
 
             $preview.html('');
