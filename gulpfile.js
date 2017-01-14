e5fcd55c3452e2e7cf45ff2f9a890c41fb89dfb8
myoungho.Pak <qkraudghgh@naver.com>
2017-01-14 12:55:20 +0900

    Modify watch dir path

--- gulpfile.js
+++ gulpfile.js
@@ -96,8 +96,8 @@ gulp.task('default', ['styles', 'scripts', 'imgCompression']);
 
 // Watch
 gulp.task('watch', ['default', 'browserSync'], () => {
-  gulp.watch(`${paths.sass}/*.scss`, ['styles']).on("change", reload);
-  gulp.watch(`${paths.js}/*.js`, ['scripts']).on("change", reload);
+  gulp.watch(`${paths.sass}/**/*.scss`, ['styles']).on("change", reload);
+  gulp.watch(`${paths.js}/**/*.js`, ['scripts']).on("change", reload);
   gulp.watch(`${paths.images}/*`, ['imgCompression']);
   gulp.watch(`${paths.templates}/**/*.html`).on("change", reload);
 });
