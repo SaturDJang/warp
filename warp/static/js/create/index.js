735ae740652c2b0cbd5f7626077c6f29a6bccb7d
Hyeon-mook Jerry Choi <chm073@gmail.com>
2017-04-15 12:15:55 +0900

    Update index.js

--- warp/static/js/create/index.js
+++ warp/static/js/create/index.js
@@ -17,35 +17,6 @@ $(() => {
     document.getElementById('id_markdown').value = editor.getValue();
   };
 
- /* $('#create_html').submit((e) => {
-    const subject = $('#id_subject').val();
-    const isPublic = document.querySelector('#id_is_public').checked;
-    const slideList = editor.getValue().split(/={5,}/g).map((v, i) => ({
-      slide_order: i,
-      markdown: v.trim(),
-      html: marked(v.trim()),
-    }));
-
-    const presentation = {
-      subject,
-      is_public: isPublic,
-      slide_list: slideList,
-    };
-
-    $.ajax({
-      url: 'create',
-      method: 'POST',
-      data: presentation,
-    }).done(() => {
-      // location.href = '/';
-    }).error((xhr, st, err) => {
-      console.log(xhr);
-      console.error(err);
-    });
-
-    e.preventDefault();
-  });
-*/
   const usageButton = new UsageButton();
   usageButton.init();
 
