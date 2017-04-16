bd683b5ebde4bff3df110a7a81b6deb2dca77f9c
Wonyoung Ju <getogrand@gmail.com>
2017-04-16 18:27:01 +0900

    Adjust slide's width and height to be 4:3 ratio

--- warp/static/js/create/index.js
+++ warp/static/js/create/index.js
@@ -4,6 +4,17 @@ $(() => {
   const editor = ace.edit('markdown_editor');
   const aceSession = editor.getSession();
 
+  const resizeSlides = () => {
+    const previewWidth = $('.preview').width();
+    $('.slide').height(previewWidth * 0.75);
+    preview.syncWithEditorCaret(editor);
+  };
+
+  const appendSlide = (content, index) => {
+    const $preview = $('.preview');
+    $preview.append(`<div class="callout secondary slide data-slide-${index}">${marked(content)}</div>`);
+  };
+
   const md2html = () => {
     const markdownContent = editor.getValue();
     const markdownSlides = markdownContent.split(/={5,}/);
@@ -11,14 +22,28 @@ $(() => {
 
     $preview.html('');
     markdownSlides.forEach((v, i) => {
-      $preview.append(`<div class="callout secondary slide data-slide-${i}">${marked(v)}</div>`);
+      appendSlide(v, i);
     });
+    resizeSlides();
+  };
+
+  $('#create_html').submit((e) => {
+    const subject = $('#id_subject').val();
+    const isPublic = document.querySelector('#id_is_public').checked;
+    const slideList = editor.getValue().split(/={5,}/g).map((v, i) => ({
+      slide_order: i,
+      markdown: v.trim(),
+      html: marked(v.trim()),
+    }));
 
     document.getElementById('id_markdown').value = editor.getValue();
   };
 
   const usageButton = new UsageButton();
   usageButton.init();
+
+  resizeSlides();
+  $(window).resize(resizeSlides);
 
   editor.setTheme('ace/theme/tomorrow_night_bright');
   aceSession.setMode('ace/mode/markdown_warp');
