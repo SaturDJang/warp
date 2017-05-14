110d36d985410e979e94edcb9a6d1a93e8088bcb
Wonyoung Ju <getogrand@gmail.com>
2017-05-14 15:29:32 +0900

    Fix wrong merge

--- warp/static/js/create/index.js
+++ warp/static/js/create/index.js
@@ -1,7 +1,7 @@
 /* global window, $, marked, vex, ace, UsageButton, preview, document, location, Prism */
 
 $(() => {
-  const editor = ace.edit('id_markdown');
+  const editor = ace.edit('markdown_editor');
   const aceSession = editor.getSession();
 
   const resizeImg = (that, naturalWidth, previewWidth) => {
@@ -77,36 +77,9 @@ $(() => {
     });
     Prism.highlightAll(); // highlights code blocks
     resizeSlides();
+
+    document.getElementById('id_markdown').value = editor.getValue();
   };
-
-  $('#create_html').submit((e) => {
-    const subject = $('#id_subject').val();
-    const isPublic = document.querySelector('#id_is_public').checked;
-    const slideList = editor.getValue().split(/={5,}/g).map((v, i) => ({
-      slide_order: i,
-      markdown: v.trim(),
-      html: marked(v.trim()),
-    }));
-
-    const presentation = {
-      subject,
-      is_public: isPublic,
-      slide_list: slideList,
-    };
-
-    $.ajax({
-      url: 'create',
-      method: 'POST',
-      data: presentation,
-    }).done(() => {
-      location.href = '/';
-    }).error((xhr, st, err) => {
-      console.log(xhr);
-      console.error(err);
-    });
-
-    e.preventDefault();
-  });
 
   const usageButton = new UsageButton();
   usageButton.init();
