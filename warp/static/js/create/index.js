2074edb1e2453fce94ca032c2df2248e71bc89bb
Wonyoung Ju <getogrand@gmail.com>
2017-05-14 16:18:32 +0900

    Refactor(rename, organize ratio constants, eslint)

--- warp/static/js/create/index.js
+++ warp/static/js/create/index.js
@@ -1,28 +1,10 @@
-/* global window, $, marked, vex, ace, UsageButton, preview, document, location */
-const RATIO = {
-  h1: 0.20,
-  h2: 0.17,
-  h3: 0.14,
-  h4: 0.11,
-  h5: 0.08,
-  p: 0.04,
-  pre: 0.04,
-  code: 0.04,
-  span: 0.04,
-  li: 0.04,
-  ul: 0.04,
-  ol: 0.04
-};
-
-const SLIDE_RATIO = 0.75;
-const PADDING_RATIO = 0.05;
-const MARGIN_RATIO = 0.01;
+/* global window, $, marked, vex, ace, UsageButton, preview, document, location, Prism */
 
 $(() => {
   const editor = ace.edit('id_markdown');
   const aceSession = editor.getSession();
 
-  const imageResiger = (that, naturalWidth, previewWidth) => {
+  const resizeImg = (that, naturalWidth, previewWidth) => {
     if (naturalWidth > previewWidth) {
       $(that).css('width', `${previewWidth}px`);
     } else {
@@ -31,27 +13,47 @@ $(() => {
   };
 
   const resizeSlides = () => {
+    const ZOOMING_RATIO = {
+      elems: {
+        h1: 0.20,
+        h2: 0.17,
+        h3: 0.14,
+        h4: 0.11,
+        h5: 0.08,
+        p: 0.04,
+        pre: 0.04,
+        code: 0.04,
+        span: 0.04,
+        li: 0.04,
+        ul: 0.04,
+        ol: 0.04,
+      },
+      slide: 0.75,
+      padding: 0.05,
+      margin: 0.01,
+    };
+
     const $slide = $('.slide');
-    const hArray = ["h1", "h2", "h3", "h4", "h5", "p", "pre", "code", "span", "li", "ul", "ol"];
     const $previewWidth = $('.preview').outerWidth();
-    const previewWidthRatioApply = $previewWidth * SLIDE_RATIO;
-    $slide.outerHeight(previewWidthRatioApply-($previewWidth * PADDING_RATIO));
-    $slide.css('padding', `${$previewWidth * PADDING_RATIO}px`);
+    const previewWidthRatioApply = $previewWidth * ZOOMING_RATIO.slide;
+    $slide.outerHeight(previewWidthRatioApply - ($previewWidth * ZOOMING_RATIO.padding));
+    $slide.css('padding', `${$previewWidth * ZOOMING_RATIO.padding}px`);
+    $slide.css('margin', `${previewWidthRatioApply * 0.03}px`);
+
     const $img = $('.slide p img');
     $img.each(function (index, element) {
-      $(element).get(0).onload = function() {
-        imageResiger(this, this.naturalWidth, $previewWidth)
+      $(element).get(0).onload = function () {
+        resizeImg(this, this.naturalWidth, $previewWidth);
       };
-      imageResiger(element, this.naturalWidth , $previewWidth)
-    });
-    hArray.forEach(size => {
-      const $font = $(`.slide ${size}`);
-      $font.css('font-size', `${previewWidthRatioApply * RATIO[size]}px`);
-      $font.css('margin-bottom', `${previewWidthRatioApply * MARGIN_RATIO}px`);
-      $('.slide pre').css('padding', `${previewWidthRatioApply * 0.02}px`);
+      resizeImg(element, this.naturalWidth, $previewWidth);
     });
 
-    $slide.css('margin', `${previewWidthRatioApply * 0.03}px`);
+    Object.keys(ZOOMING_RATIO.elems).forEach((size) => {
+      const $font = $(`.slide ${size}`);
+      $font.css('font-size', `${previewWidthRatioApply * ZOOMING_RATIO.elems[size]}px`);
+      $font.css('margin-bottom', `${previewWidthRatioApply * ZOOMING_RATIO.margin}px`);
+      $('.slide pre').css('padding', `${previewWidthRatioApply * 0.02}px`);
+    });
 
 
     Prism.highlightAll();
@@ -116,7 +118,7 @@ $(() => {
   });
 
   marked.setOptions({
-    langPrefix: "language-"
+    langPrefix: 'language-',
   });
 
   resizeSlides();
