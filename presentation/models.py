d28fa7874d7b0602eb5064d9f43b8b01674de69f
Hyeon-Mook Jerry Choi <chm073@gmail.com>
2017-05-20 18:24:28 +0900

    Add presentation queryset and model manager

--- presentation/models.py
+++ presentation/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+from django.db.models import Manager
+from django.db.models import QuerySet
 from model_utils.models import TimeStampedModel
 
 from warp.users.models import User
+
+
+class PresentationQuerySet(QuerySet):
+    def public(self):
+        return self.filter(is_public=True)
+
+    def authored_by(self, author):
+        return self.filter(author__username=author)
+
+
+class PresentationManager(Manager):
+    def get_queryset(self):
+        return PresentationQuerySet(self.model, using=self._db)
+
+    def public(self):
+        return self.get_queryset().public()
+
+    def authored_by(self, author):
+        return self.get_queryset().authored_by(author)
 
 
 class Presentation(TimeStampedModel):
@@ -9,6 +30,8 @@ class Presentation(TimeStampedModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     views = models.IntegerField(default=0)
     is_public = models.BooleanField(default=True)
+
+    objects = PresentationManager()
 
     def __str__(self):
         return self.subject
