8700bec79911987f7d34725b63ec7ddbb8bbd90f
Wonyoung Ju <getogrand@gmail.com>
2017-05-27 15:55:14 +0900

    Implement fullscreen

--- warp/static/js/detail/index.js
+++ warp/static/js/detail/index.js
@@ -1,4 +1,4 @@
-/* global $, marked, window, resizeSlides, mdElemsToHtmlElems */
+/* global $, marked, window, resizeSlides, mdElemsToHtmlElems, screenfull */
 
 $(() => {
   const slidesSelector = '.slides';
@@ -13,6 +13,18 @@ $(() => {
     $currentSlide.css('display', 'flex');
   }
 
+  function goToPrevSlide() {
+    currentSlideIndex -= 1;
+    if (currentSlideIndex < 0) currentSlideIndex = 0;
+    slideMover(currentSlideIndex);
+  }
+
+  function goToNextSlide() {
+    currentSlideIndex += 1;
+    if (currentSlideIndex > $slides.length - 1) currentSlideIndex = $slides.length - 1;
+    slideMover(currentSlideIndex);
+  }
+
   mdElemsToHtmlElems(slidesSelector);
 
   $slides.css('display', 'none');
@@ -20,14 +32,46 @@ $(() => {
   $currentSlide.css('display', 'flex');
 
   $('#prev-btn').click(() => {
-    currentSlideIndex -= 1;
-    if (currentSlideIndex < 0) currentSlideIndex = 0;
-    slideMover(currentSlideIndex);
+    goToPrevSlide();
   });
   $('#next-btn').click(() => {
-    currentSlideIndex += 1;
-    if (currentSlideIndex > $slides.length - 1) currentSlideIndex = $slides.length - 1;
-    slideMover(currentSlideIndex);
+    goToNextSlide();
+  });
+
+  $(window).keyup((e) => {
+    const keys = {
+      left: 37,
+      up: 38,
+      right: 39,
+      down: 40,
+    };
+    if (e.which === keys.left || e.which === keys.up) {
+      goToPrevSlide();
+    } else if (e.which === keys.right || e.which === keys.down) {
+      goToNextSlide();
+    }
+  });
+
+  if (screenfull.enabled) {
+    screenfull.onchange(() => {
+      if (screenfull.isFullscreen) {
+        $slides.height(window.screen.height);
+        $slides.width(window.screen.height * 1.333333);
+        // $slides.css('height', '100%');
+        // $slides.css('width', '100%');
+        resizeSlides(true, $('div#normal-view'));
+      } else {
+        // $(slidesSelector).css('height', '');
+        // $(slidesSelector).css('width', '');
+        $slides.css('height', '');
+        $slides.css('width', '');
+      }
+    });
+  }
+  $('#full-screen-btn').click(() => {
+    if (screenfull.enabled) {
+      screenfull.request($(slidesSelector)[0]);
+    }
   });
 
   resizeSlides(true, $('div#normal-view'));
